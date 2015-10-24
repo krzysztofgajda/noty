a3c3fed1b4d46c496034e69165ec533723c53272
Krzysztof Gajda <kgajda@p110-14.labpk.inf.ug.edu.pl>
2015-10-24 12:33:59 +0200

    create hello-world.c and rk.c

---
+++ hello-world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  for (i = 0; i < 30; i++) {
+
+    if (i < 10) {
+      puts("Nie będę biegał na przerwie po korytarzu");
+    } else if (i < 20) {
+      puts("Nie będę używał brzydkich wyrazów");
+    } else {
+      puts("Nie będę pluł na inne dzieci");
+    }
+
+  }
+
+  
+}
---
+++ rk.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int a = 2; //współczynnik przy x^2
+  int b = 3; //współczynik przy x
+  int c = 1; //wyraz wolny
+
+  double delta = b * b - 4 * a * c;
+  printf("delta=%f\n", delta);
+  if (delta > 0) {
+    double x1 = (-b - sqrt(delta)) / 2 * a;
+    double x2 = (-b + sqrt(delta)) / 2 * a;
+    printf("pierwiastki: %f %f\n", x1, x2);
+  } else if (delta == 0) {
+    double x1 = -b / 2 * a;
+    printf("pierwiastki: %f\n", x1);
+  } else {
+    puts("Brak pierwiastków");
+  }
+
+
+
+}
