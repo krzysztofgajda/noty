bbf5a3342d3e24748fa8840fcf7adaf9502bbb8b
Krzysztof Gajda <kgajda@p110-14.labpk.inf.ug.edu.pl>
2015-11-07 09:57:25 +0100

    update README

--- 01-budowanie_programow/5a.c
+++ 01-budowanie_programow/5a.c
@@ -1,10 +1,14 @@
+/* Program ten wykonuje konwersję ze stopni na radiany
+   według wzoru: rad = 2 * PI * deg / 360    */
+
 #include <stdio.h>
 #include <math.h>
+
 int main() {
    double deg, rad;
    printf("Podaj kat w stopniach : ");
-   scanf("...", &deg);
-   rad = ... ;  /* grep PI /usr/include/math.h */
-   printf("%lf deg = %lf rad\n", ... , ...);
+   scanf("%lf", &deg);
+   rad = 2 * 3.14159265358979323846 * deg / 360 ;  /* grep PI /usr/include/math.h */
+   printf("%.4f deg = %.4f rad\n", deg , rad);
    return 0;
 }
