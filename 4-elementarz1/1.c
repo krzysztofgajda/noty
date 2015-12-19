ed9b9ac44d8b314d8e5dcf4ac352f928981b9e30
krzysztofgajda <kgajda@sigma.ug.edu.pl>
2015-12-19 11:43:32 +0100

    add 1.c

---
+++ 4-elementarz1/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+  double n1 = 5 + (3 * 8 / 2) - 3;
+  double n2 = (0.02 * 2) + (2 * 2) - (2 / 2);
+  double n3 = 2 * 4 * (5 + 9 / 2);
+
+  printf("n1 = %f\n", n1);
+  printf("n2 = %f\n", n2);
+  printf("n3 = %f\n", n3);
+}
